f51e0ad12f24b51b9b936db48b8789b7e6538bbd
Marcos Pires <marcos.danix@gmail.com>
2015-04-13 17:45:01 +0100

    Trying stuff in angular.

--- js/irave.js
+++ js/irave.js
@@ -1,6 +1,13 @@
 (function() {
   var app = angular.module("iRave", []);
   
+  app.controller('ScreenController', [ 'dateFilter', function(dateFilter) {
+    this.display = dateFilter(new Date(), 'HH:mm');
+    this.leftButton = '←';
+    this.rightButton = '→';
+  }]);
+  
+  //The license in the bottom of the page
   app.directive('imgLicense', function(){
     return {
       restrict: 'E',
