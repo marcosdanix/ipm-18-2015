a56b5b57b2abf9238bf50f21cb4f8d73b70e2861
Marcos Pires <marcos.danix@gmail.com>
2015-04-14 20:56:34 +0100

    It's alive!

--- js/irave.js
+++ js/irave.js
@@ -1,11 +1,37 @@
 (function() {
   var app = angular.module("iRave", []);
   
-  app.controller('ScreenController', [ 'dateFilter', function(dateFilter) {
-    this.display = dateFilter(new Date(), 'HH:mm');
-    this.leftButton = '←';
-    this.rightButton = '→';
-  }]);
+  var saldo = 4.20;
+    
+  app.controller('ScreenController', 
+                 [ 'dateFilter', 
+                   'currencyFilter', 
+    function(dateFilter, currencyFilter) {
+      var Initial = function(controller) {
+        this.mainscreen = dateFilter(new Date(), "HH:mm")
+                          .concat("\nSaldo: ")
+                          .concat(currencyFilter(saldo, "€", 2));
+        this.rightButtonStyle = {'font-size': '50%'};
+        this.rightButtonText = 'MENU';
+        this.doRightButton = function() {
+          controller.state = new Menu(controller);
+        };
+      };
+      
+      var Menu = function(controller) {        
+        this.middleButtonText = '⌂';
+        this.middleButtonStyle = {'font-size': '150%'};
+        this.doMiddleButton = function() {
+          controller.state = new Initial(controller);
+        }
+        
+        this.rightButtonText = '→'
+        
+      };
+
+        
+      this.state = new Initial(this);    
+    }]);
   
   //The license in the bottom of the page
   app.directive('imgLicense', function(){
@@ -15,4 +41,4 @@
     };
   });
   
-})();
+})();   
